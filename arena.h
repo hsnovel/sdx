65d5e2f04b439dd0da2e2f6b28f323fe3efc0ecc
xcatalyst <cagankorkmaz35@gmail.com>
2023-08-03 20:10:43 +0300

    buf involves two memcpy's, it doesn't save as much as it costs

--- arena.h
+++ arena.h
@@ -14,12 +14,9 @@
 
 typedef struct {
 	unsigned char *data;	// actual arena memory
-	unsigned char *buf;	// temporary buffer, data gets stored here temporarily before flushing
 	vector backtrack;	// vector of backtrack_info, stores fragmented parts
 	size_t cap;		// capacity of data*
 	size_t used;		// used part of data*
-	size_t buf_cap;	// capacity of buf*
-	size_t buf_used;	// used part of *buf
 	int flags;		// user defined flags
 } arena;
 
@@ -27,11 +24,6 @@ typedef struct {
 	void *start;		// starting address of the fragmented part
 	size_t size;		// size of the fragmented part in bytes
 } backtrack_info;
-
-// This will be converted to a bitmask later
-enum {
-	ARENA_NOBUFFERING = 1,
-};
 
 int arena_init(arena *ar);
 void * _arena_push_size(arena *ar, size_t size);
@@ -55,13 +47,8 @@ int arena_init(arena *ar)
 	ar->buf_used = 0;
 	ar->flags = 0;
 	ar->cap = ARENA_DEFAULT_BUFFER_CAP;
-	ar->buf_cap = ARENA_DEFAULT_BUFFER_CAP;
 
 	if ((ar->data = malloc(ar->cap)) == NULL) return 0;
-	if ((ar->buf = malloc(ar->buf_cap)) == NULL) {
-		free(ar->data);
-		return 0;
-	}
 	if (!vector_init(&ar->backtrack, sizeof(backtrack_info))) {
 		free(ar->data);
 		free(ar->buf);
@@ -69,11 +56,6 @@ int arena_init(arena *ar)
 	}
 
 	return 1;
-}
-
-void arena_flush_buffer()
-{
-
 }
 
 // @Todo: Add alignement for the next pushed memory
@@ -99,71 +81,58 @@ void * _arena_push_size(arena *ar, size_t size)
 	if (ar->backtrack.index == 0)
 		return ar->data + ar->used;
 
-	// New item is too big to buffer, we might
-	// increase the buffer size but it seems
-	// unnecesarry for now, maybe will do it later
-	// @Todo: We still have to check for availabie
-	// space on backtrace before doing this
-	if ((ar->flags == 1) && (size >= ar->buf_cap))
-		return ar->data + ar->used;
-
 	// Freed memory is present, loop through backtrace
 
 	// Make sure that initial best_fit is always replaced with
 	// the first pick, set the size to highest possible value
 	backtrack_info best_fit = { .start = NULL, .size = ULONG_MAX};
 
-	// No buffering
-	if (ar->flags == 1) {
-		size_t i = 0;
-		size_t best_fit_index = 0;
-		do {
-			backtrack_info *current = vector_get(&ar->backtrack, i);
-			// Check if size is sufficent enough to hold it or
-			// vector we got is not deleted
-			if ((current->size < size) || (current->size == 0)) {
-				i++;
-				continue;
-			}
-
-			if (current->size < best_fit.size) {
-				best_fit = *current;
-				best_fit_index = i;
-			}
+	size_t i = 0;
+	size_t best_fit_index = 0;
+	do {
+		backtrack_info *current = vector_get(&ar->backtrack, i);
+		// Check if size is sufficent enough to hold it or
+		// vector we got is not deleted
+		if ((current->size < size) || (current->size == 0)) {
 			i++;
-		} while (i < ar->backtrack.index);
-
-		// Nowhere to fit the current allocation
-		// push it to the top
-		if (best_fit.start == NULL)
-			return ar->data + ar->used;
-
-		// Good fit is found, check memory left over
-		// and replace the current current best fit
-		// with the space left over
-		size_t space_leftover = best_fit.size - size;
-
-		// If it fits perfectly then do not replace it
-		// instead delete the entry and return the space
-		if (space_leftover == 0) {
-			vector_free_item(&ar->backtrack, best_fit_index);
-			return best_fit.start;
+			continue;
 		}
 
-		// If there is a space left, replace the
-		// current fragmentation with the new one
-		// that points to the beggining of the new
-		// fragmentation part
-		backtrack_info fragmented_part = {
-			.start = (unsigned char*)best_fit.start + size,
-			.size = space_leftover,
-		};
+		if (current->size < best_fit.size) {
+			best_fit = *current;
+			best_fit_index = i;
+		}
+		i++;
+	} while (i < ar->backtrack.index);
 
-		vector_replace_item(&ar->backtrack, best_fit_index, &fragmented_part);
+	// Nowhere to fit the current allocation
+	// push it to the top
+	if (best_fit.start == NULL)
+		return ar->data + ar->used;
+
+	// Good fit is found, check memory left over
+	// and replace the current current best fit
+	// with the space left over
+	size_t space_leftover = best_fit.size - size;
+
+	// If it fits perfectly then do not replace it
+	// instead delete the entry and return the space
+	if (space_leftover == 0) {
+		vector_free_item(&ar->backtrack, best_fit_index);
 		return best_fit.start;
 	}
 
-	return NULL;
+	// If there is a space left, replace the
+	// current fragmentation with the new one
+	// that points to the beggining of the new
+	// fragmentation part
+	backtrack_info fragmented_part = {
+		.start = (unsigned char*)best_fit.start + size,
+		.size = space_leftover,
+	};
+
+	vector_replace_item(&ar->backtrack, best_fit_index, &fragmented_part);
+	return best_fit.start;
 }
 
 void _arena_free(arena *ar, void *ptr, size_t size)
@@ -174,11 +143,6 @@ void _arena_free(arena *ar, void *ptr, size_t size)
 
 	// clear the memory
 	memset(ptr, 0, size);
-}
-
-void _arena_get()
-{
-	arena_flush_buffer();
 }
 
 #endif
