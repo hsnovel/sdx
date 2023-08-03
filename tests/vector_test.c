b1f922d013689c6f2b91bcebbcf389d26c6ec6f3
xcatalyst <cagankorkmaz35@gmail.com>
2023-08-03 20:01:26 +0300

    todo should be done, there might be bugs gonna check later

--- tests/vector_test.c
+++ tests/vector_test.c
@@ -16,7 +16,7 @@ int main()
 	assert(*((int*)vector_get(&vec, 99999)) == 99999 && "Assertion failed checking index 99999");
 
 	vector_free_item(&vec, 20);
-	assert(*((int*)vector_get(&vec, 20)) == 0 && "Assertion failed checkig index 20");
+	assert(((int*)vector_get(&vec, 20)) == NULL && "Assertion failed checkig index 20");
 
 	int newdata = 12;
 	vector_replace_item(&vec, 25, &newdata);
--- vector.h
+++ vector.h
@@ -14,20 +14,24 @@
 
 #define VECTOR_INITIAL_CAP 256
 #define VECTOR_INITIAL_BACKTRACK 32
+#define VECTOR_INITIAL_DELETED_ITEM_CAP 16
 
 typedef struct {
-	size_t cap;   /* in bytes */
-	size_t index; /* counter in numbers */
-	size_t itemsize; /* in bytes */
-	unsigned char *data;
+	size_t cap;		// in bytes
+	size_t index;		// counter in numbers
+	size_t itemsize;	// in bytes
+	unsigned char *data;	// actualy data
+	int *deleted_items;	// funny but we need a vector inside a vector
+	int deleted_item_cap;	// deleted item vector's capacity as count
+	int deleted_item_index; // deleted item vector's current index
 } vector;
 
 int vector_init(vector *vector, int size);
 int vector_push(vector *vector, void *data);
-void *vector_get(vector *vector, int index);
+void *vector_get(vector *vector, size_t index);
 void vector_pop(vector *vector);
 
-#endif /* VECTOR_H */
+#endif // VECTOR_H
 
 #ifdef VECTOR_IMPLEMENTATION
 
@@ -36,19 +40,32 @@ int vector_init(vector *vector, int size)
 	vector->cap = VECTOR_INITIAL_CAP;
 	vector->index = 0;
 	vector->itemsize = size;
-	vector->data = malloc(VECTOR_INITIAL_CAP);
-	if (vector->data == NULL)
+	vector->deleted_item_index = 0;
+	vector->deleted_item_cap = VECTOR_INITIAL_DELETED_ITEM_CAP;
+	if ((vector->data = malloc(VECTOR_INITIAL_CAP)) == NULL)
 		return 0;
+	if ((vector->deleted_items = malloc(VECTOR_INITIAL_DELETED_ITEM_CAP))) {
+		free(vector->data);
+		return 0;
+	}
+
+	memset(vector->deleted_items, -1, vector->deleted_item_cap);
+
 	return 1;
 }
 
+// Unfortunately we cannot replace deleted items with
+// the data* as it will screw the indexing. For that
+// Whenever vector_free_item() is used it will
+// leave a fragmentation behind. For a non
+// fragmented version please check arena.h
 int vector_push(vector *vector, void *data)
 {
 	if (vector->cap <= (vector->index * vector->itemsize) + vector->itemsize) {
 		size_t newcap = vector->cap * 2;
 		unsigned char *tmp = realloc(vector->data, newcap);
 		if (tmp == NULL) {
-			tmp = malloc(vector->cap * 2);
+			tmp = malloc(newcap);
 			if (tmp == NULL)
 				return 0;
 			memcpy(tmp, vector->data, vector->index * vector->itemsize);
@@ -64,21 +81,48 @@ int vector_push(vector *vector, void *data)
 
 void vector_replace_item(vector *vector, size_t index, void *data)
 {
+	// Place the data to the specified index
 	memcpy(vector->data + (index * vector->itemsize), data, vector->itemsize);
+
+	// Remove the item from deleted index
+	vector->deleted_items[index] = 0;
 }
 
-// @Todo: store a vector of deleted items, when vector_get
-// is called, check if deleted items vector has that index
-// if so retur NULL otherwise proceed normally
-void *vector_get(vector *vector, int index)
+void *vector_get(vector *vector, size_t index)
 {
 	assert((size_t)vector->index >= (size_t)index);
+	for (int i = 0; i < vector->deleted_item_cap; i++) {
+		if (index == (size_t)vector->deleted_items[i])
+			return NULL;
+	}
 	return vector->data + (index * vector->itemsize);
 }
 
-void vector_free_item(vector *vector, int index)
+// We maybe hash this values later.
+int vector_free_item(vector *vector, size_t index)
 {
+	// Clear deleted items
 	memset(vector->data + (index * vector->itemsize), 0, vector->itemsize);
+
+	// Check deleted item array to see if there is enough space
+	if (vector->deleted_item_cap < vector->deleted_item_index) {
+		size_t newcap = vector->deleted_item_cap * 2 * sizeof(int);
+		unsigned char *tmp = realloc(vector->data, newcap);
+		if (tmp == NULL) {
+			tmp = malloc(newcap);
+			if (tmp == NULL)
+				return 0;
+			memcpy(tmp, vector->deleted_items, vector->deleted_item_index * sizeof(int));
+			free(vector->deleted_items);
+		}
+		vector->cap = newcap;
+		vector->data = tmp;
+	}
+
+	// Add the index to the deleted items vector
+	vector->deleted_items[vector->deleted_item_index] = index;
+	vector->deleted_item_index++;
+	return 1;
 }
 
 void vector_free(vector *vector)
@@ -95,4 +139,4 @@ void vector_pop(vector *vector)
 	vector->index--;
 }
 
-#endif /* VECTOR_IMPLEMENTATION */
+#endif // VECTOR_IMPLEMENTATION
