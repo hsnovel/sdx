3fc168646d11cdbeb5e40cbe2821b5ec2eeb07dc
xcatalyst <cagankorkmaz35@gmail.com>
2023-08-04 06:56:03 +0300

    add header

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir -p tmp
-gcc -std=c2x -Wall -Wextra -g3 vector_test.c -o tmp/vector_test
+# gcc -std=c99 -Wall -Wextra -g3 vector_test.c -o tmp/vector_test
+gcc -std=c99 -Wall -Wextra -g3 string_view_test.c -o tmp/string_view
