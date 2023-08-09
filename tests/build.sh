39fe1eb4f8975f366466984d4597284c283be49d
xcatalyst <cagankorkmaz35@gmail.com>
2023-08-09 07:32:18 +0300

    update test

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p tmp
-# gcc -std=c99 -Wall -Wextra -g3 vector_test.c -o tmp/vector_test
+gcc -std=c99 -Wall -Wextra -g3 array_test.c -o tmp/array_test
 gcc -std=c99 -Wall -Wextra -g3 string_view_test.c -o tmp/string_view
+# gcc -std=c99 -Wall -Wextra -g3 arena_test.c -o tmp/arena
