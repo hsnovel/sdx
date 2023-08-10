975e31f814b5d8e6d09ca533b3252c6f9c598af3
xcatalyst <cagankorkmaz35@gmail.com>
2023-08-11 00:16:23 +0300

    delete tests, they are in main.c

--- tests/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir -p tmp
-gcc -std=c99 -Wall -Wextra -g3 array_test.c -o tmp/array_test
-gcc -std=c99 -Wall -Wextra -g3 string_view_test.c -o tmp/string_view
-gcc -std=c99 -Wall -Wextra -g3 arena_test.c -o tmp/arena
-gcc -std=c99 -Wall -Wextra -g3 strvec_test.c -o tmp/strvec
